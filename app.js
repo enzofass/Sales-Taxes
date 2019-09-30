9d8707049e04e114b8a1540c08b0cc890e49c7d4
enzofass <zoefassioli@gmail.com>
2019-09-30 18:56:11 -0400

    logic working as expected, start unit testing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-function purchaseItems(arr) {
+purchaseItems = arr => {
+  const itemizedReceipt = [];
   arr.forEach(e => {
     // substring of items that are sales tax exempt
     const salesTaxExempt = [
@@ -8,32 +9,32 @@ function purchaseItems(arr) {
       "box of imported chocolates",
       "packet of headache pills"
     ];
+
     //  check if item string includes the word imported
     if (e.item.includes("imported")) {
-      console.log(`This ${e.item} is imported`);
       // check if the imported item is sales tax exempt
       if (salesTaxExempt.some(substring => e.item.includes(substring))) {
-        console.log(`This ${e.item} is tax exempt`);
-        console.log(e.price);
-        console.log(getTax(e, 0.05));
+        itemizedReceipt.push(applyTax(e, 0.05));
       } else {
-        console.log(`This ${e.item} is taxable`);
-        console.log(e.price);
-        console.log(getTax(e, 0.15));
+        itemizedReceipt.push(applyTax(e, 0.15));
       }
-    } else {
+    }
+    // if item is not imported check if tax exempt
+    else {
       if (salesTaxExempt.some(substring => e.item.includes(substring))) {
-        console.log(`This ${e.item} is tax exempt`);
-        console.log(e.price);
-        console.log(getTax(e, 0));
+        itemizedReceipt.push(applyTax(e, 0));
       } else {
-        console.log(`This ${e.item} is taxable`);
-        console.log(e.price);
-        console.log(getTax(e, 0.1));
+        itemizedReceipt.push(applyTax(e, 0.1));
       }
     }
   });
-}
+
+  // call function that calculates totals
+  getCount(itemizedReceipt);
+  // Merge totals to array of item objects
+  const output = arr.concat(totalTax);
+  return console.log("Output:", output);
+};
 
 // function Round to nearest .05
 roundToNearestHund = num => {
@@ -42,14 +43,27 @@ roundToNearestHund = num => {
 };
 
 // function that applies tax accordingly
-getTax = (item, taxRate) => {
-  // calc tax
-  const calcTax = roundToNearestHund(item.price * taxRate);
-  console.log("calcTax", calcTax, "item.price", item.price);
-  const result = Number(calcTax) + Number(item.price);
-  return [result.toFixed(2), calcTax];
+applyTax = (item, taxRate) => {
+  // tax calucated
+  const taxCalc = roundToNearestHund(item.price * taxRate);
+  const result = Number(taxCalc) + Number(item.price);
+  item.price = result.toFixed(2);
+  return { salesTax: taxCalc, total: item.price };
 };
 
+// Get itemized list of items and calcualte gran total and tax totals
+getCount = itemizedReceipt => {
+  itemizedReceipt.reduce((previousVal, currentVal) => {
+    sumTax = Number(previousVal.salesTax) + Number(currentVal.salesTax);
+    sumTotal = Number(previousVal.total) + Number(currentVal.total);
+    return (totalTax = {
+      salesTax: sumTax.toFixed(2),
+      total: sumTotal.toFixed(2)
+    });
+  });
+};
+
+// inputs provided
 const input1 = [
   { item: "book", qty: 1, price: 12.49 },
   { item: "music", qty: 1, price: 14.99 },
@@ -68,6 +82,10 @@ const input3 = [
   { item: "box of imported chocolates", qty: 1, price: 11.25 }
 ];
 
+// function executed to print receipt
 purchaseItems(input1);
+purchaseItems(input2);
+purchaseItems(input3);
 
-module.exports = purchaseItems;
+// export function
+module.exports = { purchaseItems, roundToNearestHund, getCount, applyTax };
--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,40 @@
-const sum = require("../app");
+const {
+  purchaseItems,
+  roundToNearestHund,
+  applyTax,
+  getCount
+} = require("../app");
 
-describe("sum", () => {
-  test("tells a gigasecond anniversary since midnight", () => {
-    expect(sum(1, 2)).toEqual(3);
+describe("roundToNearestHund", () => {
+  test("accepts a number and rounds to nearest .05", () => {
+    const roundNum = roundToNearestHund(1.57);
+    expect(roundNum).toEqual(1.60);
   });
 });
+
+describe("applyTax", () => {
+  test("accepts a number and rounds to nearest .05", () => {
+    const roundNum = roundToNearestHund(1.57);
+    expect(roundNum).toEqual(1.60);
+  });
+});
+
+describe("roundToNearestHund", () => {
+  test("accepts a number and rounds to nearest .05", () => {
+    const roundNum = roundToNearestHund(1.57);
+    expect(roundNum).toEqual(1.60);
+  });
+});
+
+describe("purchaseItems", () => {
+  test("accepts an array of item objects and applies tax when nessesary", () => {
+    const output1 = [
+      { item: "book", qty: 1, price: 12.49 },
+      { item: "music", qty: 1, price: 16.49 },
+      { item: "chocolate bar", qty: 1, price: 0.85 },
+      { salesTax: 1.5, total: 29.83 }
+    ];
+    expect(output1);
+  });
+});
+
